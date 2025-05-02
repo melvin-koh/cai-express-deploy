d5c63daaab42062513776fb1f016f787caa033e4
Melvin Koh <ckoh@cloudera.com>
2025-05-02 10:57:38 +0800

    Update run.sh
    Add parameters for IPA

--- run.sh
+++ run.sh
@@ -1,25 +1,26 @@
-export NODES="ccycloud-1.mktest.root.comops.site ccycloud-2.mktest.root.comops.site"
+export NODES="ccycloud-1.mktest2.root.comops.site ccycloud-2.mktest2.root.comops.site"
+export NODE_IPA="ccycloud-1.mktest2.root.comops.site"
 export CLUSTER_NAME="meltest"
 export PAYWALL_USER=XXXX
 export PAYWALL_PASSWORD=XXXXX
 export LICENSE_FILE=/root/license.txt
 
-
 ./setup-cluster.sh \
     --cluster-name=${CLUSTER_NAME} \
     --nodes-base="${NODES}" \
+    --node-ipa="${NODE_IPA}" \
     --node-user=root \
     --node-password=cloudera \
     --license-file=${LICENSE_FILE} \
     --paywall-username=${PAYWALL_USER} \
     --paywall-password=${PAYWALL_PASSWORD} \
-    --cluster-type= \
+    --cluster-type=cml-compute \
     --cm-version=7.11.3.24 \
-    --cdh-version=7.1.9.1010 \
-    --cm-repo=https://archive.cloudera.com/p/cm7/7.11.3.24/redhat8/yum/ \
-    --cdh-repo=https://archive.cloudera.com/p/cdh7/7.1.9.1010/parcels/ \
+    --cdh-version=7.1.9.1015 \
+    --cm-repo=https://archive.cloudera.com/p/cm7/7.11.3.28/redhat8/yum/ \
+    --cdh-repo=https://archive.cloudera.com/p/cdh7/7.1.9.1015/parcels/ \
     --default-admin-user=cdpadmin \
     --default-password=P@ssw0rd \
     --realm=MELTEST.COM \
     --install-repo-url=https://github.com/melvin-koh/cldr-playbook/archive/refs/heads/main.zip \
-
+    --debug=true
